ecdd826b2bae713efad97bd64cfb8d4a320363f4
DahyunMoon <21600231@handong.edu>
2020-04-30 15:27:37 +0900

    Practice 3

---
+++ C03.c
@@ -0,0 +1,26 @@
+/*
+	<아파트 평형 계산>
+	아파트의 분양 면적을 제곱미터(m^2)단위로 입력받아
+	이 값을 평형 단위의 값으로 변환하여 출력하라.
+	단, 평형 수 = 제곱미터 / 3.305 로 계산한다.
+
+	변수는 다음과 같이 사용하라.
+	float m2_area; // 면적(제곱미터)
+	float pyung_area; // 면적(평수)
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+	float m2_area; // 면적 (제곱미터)
+	float pyung_area; // 면적 (평수)
+
+	printf("m2_area? ");
+	scanf("%f", &m2_area);
+
+	pyung_area = m2_area / 3.305;
+	printf("pyung_area is %.1f\n", pyung_area);
+
+	return 0;
+}
