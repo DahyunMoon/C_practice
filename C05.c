8aef45d0220bb34d1547dd691864f59564f6812d
DahyunMoon <21600231@handong.edu>
2020-05-01 14:53:52 +0900

    Practice 5_modified

--- C05.c
+++ C05.c
@@ -21,7 +21,7 @@ int main(void){
 	int gigabytes; // 용량(기가바이트 단위)
 	int megabytes; // 용량(메가바이트 단위)
 	int kilobytes; // 용량(킬로바이트 단위)
-	unsigned int bytes; // 용량(바이트 단위)
+	unsigned int bytes; // 용량(바이트 단위) -> 부호가 없는 정수를 10진수 형태로 출력
 
 	printf("File capacity in gigabytes? ");
 	scanf("%d", &gigabytes);
@@ -31,6 +31,7 @@ int main(void){
 	bytes = kilobytes*1024;
 
 	printf("File capacity is \n%d megabytes,\n%d kilobytes,\n%u bytes.\n", megabytes, kilobytes, bytes);
-
+	// %u type을 쓰면 허용범위 초과한 숫자가 (-)로 뜨는 것 방지 가능! 
+	// gigabytes가 20일 때부터 bytes가 0으로 뜨는 문제 발생. Why?
 	return 0;
 }
