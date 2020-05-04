75a7724bcd865edcb67abd8283601258f8071726
DahyunMoon <21600231@handong.edu>
2020-05-04 18:27:44 +0900

    Practice 10_not yet

---
+++ C10.c
@@ -0,0 +1,36 @@
+/*
+	<비만 판정>
+	신장(cm단위)과 체중(kg단위)를 입력받은 후, 비만여부를 판정하여 출력하라.
+	단, 비만여부는 다음 비만도 수치가 25이상인 경우레 "비만"으로 판단한다.
+	비만도수치 = 체중(kg) / (신장(m)의 제곱) 으로 계산한다.
+	
+	*이 때, 신장은 미터 단위로 환산해야 함을 유의하라.
+
+	변수는 다음과 같이 사용하라.
+	int height, weight; // 신장(cm), 체중(kg)
+	float bmi; // 비만도 수치
+*/
+
+#include <stdio.h>
+
+int main(){
+	int height, weight; // 신장(cm), 체중(kg)
+	float bmi; // 비만도 수치
+	float m_height = height*0.01;
+
+	printf("height? ");
+	scanf("%d", &height);
+	printf("weight? ");
+	scanf("%d", &weight);
+
+	bmi = weight / (height*height*0.00001) ;	
+	printf("BMI = %.f\n", bmi);
+	printf("m_height = %.f\n", m_height);
+
+	if(bmi < 25)
+		printf("You are not overweight.\n");
+	else
+		printf("You are overweight.\n");
+
+	return 0;
+}
