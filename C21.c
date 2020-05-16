68257dad353dd5536af736d64632e847ccb046ea
DahyunMoon <21600231@handong.edu>
2020-05-16 19:14:04 +0900

    Practice 21_middle

--- C21.c
+++ C21.c
@@ -14,11 +14,35 @@
 
 int main(){
 	int num1, num2; // 입력받은 두 수
-	int i, double1, double2; // 반복문을 위한 변수
-	int num[] = (int*)malloc(sizeof(int));
+	int i// 반복문을 위한 변수
+	int cal1[] = (int*)malloc(sizeof(int));
+	int cal2[] = (int*)malloc(sizeof(int));
+	int final[] = (int*)malloc(sizeof(int));
+	int count1 = 0; 
+	int count2 = 0;
 
 	printf("2개의 숫자를 입력: ");
 	scanf("%d %d", &num1, &num2);
+
+	for(i = 0; num1 < 100; i++){
+		cal1[i] = num1;
+		count1++;
+		num1 = num1*(i+2);
+	}
+	for(i = 0; num2 < 100; i++){
+		cal2[i] = num2;
+		count2++;
+		num2 = num2*(i+2);
+	}
+	
+	int size = count1 + count2;
+
+	for(i = 0; count1!=0 && count2!=0; i++){
+	//int array 비교 후 작은거 먼저 final array에 넣어주기
+	}
+
+	for(i = 0; i < size; i++)
+		printf("%d\n", final[i]);
 
 	return 0;
 }
